6a524b78e142cdad2eed01101251c3f29caee40b
hementhu <87664415+hementhu@users.noreply.github.com>
2021-07-20 17:04:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./text.css";
+
+export default function App() {
+  return (
+    <div>
+      <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+</div>
+    </div>
+  );
+}
